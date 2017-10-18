769f4f7fcaa5238c86911870b6463cca844494e5
u0861355 <u0861355@eae-d-018>
2017-10-17 20:49:53 -0600

    Add project files.

---
+++ WindowsFormsApplication1/Shape.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Shape
+    {
+
+        public Vector2[] shapePoints =
+        {
+            new Vector2(10,10), new Vector2(10,20),
+            new Vector2(10,20), new Vector2(20,20),
+            new Vector2(20,20), new Vector2(20,10),
+            new Vector2(20,10), new Vector2(10,20)
+        };
+
+        public void DrawStuff(Graphics g, Vector2 location)
+        {
+            for (int i = 0; i < shapePoints.Length; i+=2)
+            {
+                g.DrawLine(Pens.Red, shapePoints[i].GetX(), shapePoints[i].GetY(), shapePoints[i+1].GetX(), shapePoints[i+1].GetY());
+            }
+
+            //Font drawFont = new Font("Arial", 16);
+            //g.DrawString("Mittens", drawFont, Brushes.DeepPink, location.GetX(), location.GetY());
+            //hellosdf
+        }
+    }
+}
---
+++ WindowsFormsApplication1/Vector2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Vector2
+    {
+        private float x;
+        private float y;
+
+        public Vector2(float x = 0, float y = 0)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Vector2() : this(0,0)
+        {
+
+        }
+
+        public void SetX(float x)
+        {
+            this.x = x;
+        }
+        public void SetY(float y)
+        {
+            this.y = y;
+        }
+        public float GetX()
+        {
+            return x;
+        }
+        public float GetY()
+        {
+            return y;
+        }
+
+        public float GetLength()
+        {
+            return (float)Math.Sqrt(9);
+        }
+
+        public static Vector2 operator +(Vector2 v1, Vector2 v2)
+        {
+            Vector2 result = new Vector2(v1.x+v2.x, v1.y+v2.y);
+            return result;
+        }
+
+        public static Vector2 operator *(Vector2 v1, Vector2 v2)
+        {
+            Vector2 result = new Vector2((v1.x * v2.y) - (v1.y * v2.x));
+            return result;
+        }
+    }
+}
